81e4baef5a18acedfd344e8f6b80235e6a6ad7e7
流年 <2538943625@qq.com>
2017-09-19 17:30:09 +0800

    在更新

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -2,18 +2,24 @@
  * Created by Administrator on 2017/9/19 0019.
  */
 
-export default {
-  async reduce ({ commit, state }) {
-    try {
-      if (state.count !== '' && state.count !== 0) {
-        console.log(state.count)
-        state.count--
-      } else {
-        alert('count的初始值大于20或者小于20')
-      }
-    } catch (err) {
-      throw new Error('count的初始值大于20或者小于20')
-    }
-  }
-}
+// export default {
+//   async reduce ({ commit, state }) {
+//     try {
+//       if (state.count !== '' && state.count !== 0) {
+//         console.log(state.count)
+//         state.count--
+//       } else {
+//         alert('count的初始值大于20或者小于20')
+//       }
+//     } catch (err) {
+//       throw new Error('count的初始值大于20或者小于20')
+//     }
+//   }
+// }
+import {
+  reduce
+} from './data'
 
+export default {
+  reduce
+}
